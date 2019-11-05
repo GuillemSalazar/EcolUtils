2b57a8a8b76eba0852c56ce30f93d92c30320161
guillems <guillems@ethz.ch>
2019-11-05 12:21:54 +0100

    Add SumsOfSqs, MeanSqs and F.Model to adonis.pair

--- R/EcolUtils_functions.R
+++ R/EcolUtils_functions.R
@@ -66,12 +66,19 @@ adonis.pair<-function(dist.mat,Factor,nper=1000,corr.method="fdr"){
   comb.fact<-combn(levels(Factor),2)
   pv<-NULL
   R2<-NULL
+  SS<-NULL
+  MeanSqs<-NULL
+  F.Model<-NULL
   for (i in 1:dim(comb.fact)[2]){
     model.temp<-adonis(as.dist(as.matrix(dist.mat)[Factor==comb.fact[1,i] | Factor==comb.fact[2,i],Factor==comb.fact[1,i] | Factor==comb.fact[2,i]])~Factor[Factor==comb.fact[1,i] | Factor==comb.fact[2,i]],permutations=nper)
     pv<-c(pv,model.temp$aov.tab[[6]][1])
-    R2<-c(R2,model.temp$aov.tab$R2[1])}
+    R2<-c(R2,model.temp$aov.tab$R2[1])
+    SS<-c(SS,model.temp$aov.tab[[2]][1])
+    MeanSqs<-c(MeanSqs,model.temp$aov.tab[[3]][1])
+    F.Model<-c(F.Model,model.temp$aov.tab[[4]][1])
+    }
   pv.corr<-p.adjust(pv,method=corr.method)
-  data.frame(combination=paste(comb.fact[1,],comb.fact[2,],sep=" <-> "),R2=R2,P.value=pv,P.value.corrected=pv.corr)}
+  data.frame(combination=paste(comb.fact[1,],comb.fact[2,],sep=" <-> "),SumsOfSqs=SS,MeanSqs=MeanSqs,F.Model=F.Model,R2=R2,P.value=pv,P.value.corrected=pv.corr)}
 
 #' Specialist/Generalist classification of OTUs based on niche width and permutation algorithms
 #'
